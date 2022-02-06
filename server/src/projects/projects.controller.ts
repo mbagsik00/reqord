3cd5a04657806049dc46f4e14c32f8e67eb439f5
Melvin Bagsik <mbagsik00@gmail.com>
2022-02-06 14:13:33 +1300

    Update references

--- server/src/projects/projects.controller.ts
+++ server/src/projects/projects.controller.ts
@@ -1,9 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('projects')
 export class ProjectsController {
+  // To get request details
+  // @Request() req:Request
+
+  // To pass params
+  // @Param('id') id: string
+
+  // To pass payload body
+  // @Body() payload: ProjectPayload
+
+  // To add child path in the route
+  // @Get('active')
+  // Output: projects/active
+
   @Get()
   getProjects(): any {
-    return 'list of projects'
+    return 'list of projects';
+  }
+
+  @Get(':id')
+  getProjectById(@Param('id') id: string): any {
+    return `project with id ${id}`;
   }
 }
